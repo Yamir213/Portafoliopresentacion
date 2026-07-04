c2b31cb1fb4793f7b21a8adb7cf315b63f29d67d
Yamir213 <yamircheccllorojas@gmail.com>
2026-07-04 15:34:00 -0500

    separando todo p

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        mono: ['JetBrains Mono', 'ui-monospace', 'monospace'],
+      },
+      colors: {
+        accent: { DEFAULT: '#a3e635', soft: '#84cc16' },
+      },
+    },
+  },
+  plugins: [],
+};
